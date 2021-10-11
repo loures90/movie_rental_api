a0a3fcfcc4a72509d71fc2a73e3886cd623941f7
Fernando <Fernando>
2021-10-11 18:07:44 -0300

    create UserModel

---
+++ src/_fixtures/users.ts
@@ -0,0 +1,9 @@
+import { UsersModel } from '../models/users'
+
+export const fixUser: UsersModel = {
+    id: 'any_id',
+    name: 'any_name', 
+    login: 'any_login', 
+    email: 'any_email', 
+    password: 'any_password', 
+}
